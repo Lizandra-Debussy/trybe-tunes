3ad73dbff483d6e4f60c686deea670cf3ecf0e6a
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-07-01 16:14:00 -0300

    Requisito 1 concluído

---
+++ src/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile">Profile</div>
+    );
+  }
+}
+
+export default Profile;
