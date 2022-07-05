efa98979a603d53f9ffae9ce579415505085b93c
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-07-05 16:13:00 -0300

    Requisito 5 concluído

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artist: '',
+      buttonDisabled: true,
+
+    };
+  }
+
+  inputChange = ({ target }) => {
+    const { value } = target;
+    const characters = 2;
+    this.setState({
+      artist: value,
+      buttonDisabled: value.length < characters,
+    });
+  }
+
   render() {
+    const { artist, buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         Search
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            value={ artist }
+            onChange={ this.inputChange }
+            placeholder="Nome do Artista"
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ buttonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
